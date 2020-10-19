15cee07ca8233d0f8a27be534d17a0513e8cf0c0
Mohit <mohit2002ss@gmail.com>
2020-10-19 15:56:35 +0530

    Update root.bat

--- root.bat
+++ root.bat
@@ -1,13 +1,16 @@
  
+ 
 @ECHO OFF
 ECHO NOTE* - EVERYTHING YOU DO NOW ONWARDS IS AT YOUR OWN RISK*
 ECHO ENABLE USB Debugging
 PAUSE
 adb reboot bootloader
 PAUSE
+ECHO Checking Devices
 fastboot devices
 ECHO If you device is not showing contact in group.
 PAUSE
+ECHO Disablibg dm-verity
 fastboot --disable-verity --disable-verification flash vbmeta vbmeta.img
 PAUSE
 
@@ -32,15 +35,19 @@ ECHO "%choice%" is not valid, try again
 ECHO.
 goto start
 :41
+ECHO Flashing Magisk for B41...
 fastboot flash boot magisk_41.img
 goto end
 :45
+ECHO Flashing Magisk for B45...
 fastboot flash boot magisk_45.img
 goto end
 :47
+ECHO Flashing Magisk for B47...
 fastboot flash boot magisk_47.img
 goto end
 :33
+ECHO Flashing Magisk for B33...
 fastboot flash boot magisk_33.img
 goto end
 :end
